c1421c88b84671f33a142bceee33dc2cb940d713
Code With Stein <codewithstein@Steins-MBP.home>
2021-02-07 21:02:21 +0100

    part 1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,59 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Invoicely</strong></router-link>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <template v-if="$store.state.isAuthenticated">
+            <router-link to="/dashboard" class="navbar-item">Dashboard</router-link>
+          </template>
+
+          <template v-else>
+            <router-link to="/" class="navbar-item">Home</router-link>
+
+            <div class="navbar-item">
+              <div class="buttons">
+                <router-link to="/sign-up" class="button is-success"><strong>Sign up</strong></router-link>
+                <router-link to="/log-in" class="button is-light">Log in</router-link>
+              </div>
+            </div> 
+          </template>
+        </div>
+      </div>
+    </nav>
+
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2021</p>
+    </footer>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import axios from 'axios'
 
-#nav {
-  padding: 30px;
+  export default {
+    name: 'App',
+    beforeCreate() {
+      this.$store.commit('initializeStore')
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+      const token = this.$store.state.token
 
-    &.router-link-exact-active {
-      color: #42b983;
+      if (token) {
+        axios.defaults.headers.common['Authorization'] = "Token " + token
+      } else {
+        axios.defaults.headers.common['Authorization'] = ""
+      }
     }
   }
-}
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma';
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,30 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: {
+      username: ''
+    },
+    isAuthenticated: false,
+    token: ''
   },
   mutations: {
+    inititializeStore(state) {
+      if (localStorage.getItem('token')) {
+        state.token = localStorage.getItem('token')
+        state.isAuthenticated = true
+      } else {
+        state.token = ''
+        state.isAuthenticated = false
+      }
+    },
+    setToken(state, token) {
+      state.token = token
+      state.isAuthenticated = true
+    },
+    removeToken(state) {
+      state.token = ''
+      state.isAuthenticated = false
+    }
   },
   actions: {
   },
